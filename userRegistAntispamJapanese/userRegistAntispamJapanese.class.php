317682b16639a1bef7200a305516a94feec7a1a1
nao-pon <hypweb@gmail.com>
2014-02-18 17:44:25 +0900

    update userRegistAntispamJapanese

--- userRegistAntispamJapanese/userRegistAntispamJapanese.class.php
+++ userRegistAntispamJapanese/userRegistAntispamJapanese.class.php
@@ -7,6 +7,16 @@ class userRegistAntispamJapanese extends XCube_ActionFilter
 	// 言語定数オーバーライドシステムを利用する場合には適切にディレクトリ名を指定する
 	private $mydirname = 'd3forum';
 	
+	// 使用する辞書ファイル (空文字は d3forum 標準) d3forum 0.88.3 以降で有効
+	// 絶対パス、またはファイル名のみ。ファイル名のみの場合は XOOPS_TRUST_PATH/modules/d3forum/class をディレクトリとする
+	private $dicFile = '';
+	
+	// キャプション (空文字は d3forum 標準)
+	private $caption = '';
+	
+	// 認証エラー時のメッセージ (空文字は d3forum 標準)
+	private $error = '';
+	
 	function postFilter() {
 		$this->mRoot->mDelegateManager->add('Legacy.Event.RegistUser.Validate', array($this, 'registUserValidate'));
 		$this->mRoot->mDelegateManager->add('Legacy.Event.RegistUser.SetValidators', array($this, 'registUserSetValidator'));
@@ -16,8 +26,9 @@ class userRegistAntispamJapanese extends XCube_ActionFilter
 		if (include_once(XOOPS_TRUST_PATH.'/modules/d3forum/class/D3forumAntispamJapanese.class.php')) {
 			$this->loadLang();
 			$validator = new D3forumAntispamJapanese();
+			$validator->setVar('dictionary_file', $this->dicFile);
 			if ( !$validator->checkValidate() ) {
-				$actionForm->addErrorMessage(_MD_D3FORUM_ERR_JAPANESEINCORRECT);
+				$actionForm->addErrorMessage( $this->error? $this->error : _MD_D3FORUM_ERR_JAPANESEINCORRECT );
 			}
 		}
 	}
@@ -26,9 +37,13 @@ class userRegistAntispamJapanese extends XCube_ActionFilter
 		if (include_once(XOOPS_TRUST_PATH.'/modules/d3forum/class/D3forumAntispamJapanese.class.php')) {
 			$this->loadLang();
 			$validator = new D3forumAntispamJapanese();
+			$validator->setVar('dictionary_file', $this->dicFile);
 			$html = $validator->getHtml4Assign();
 			$html = $html['html_in_form'];
 			list($caption, $element) = explode('</label>', $html);
+			if ($this->caption) {
+				$caption = preg_replace('/(<label for="antispam_yomigana">).*:/s', '$1'.$this->caption.':', $caption);
+			}
 			$validators[] = array(
 				'caption' => $caption.'</label>',
 				'element' => $element
